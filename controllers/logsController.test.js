08e87bf755e74208e611ba003e55dd3f392bef6a
K. Rafalski <kr2174@caa.columbia.edu>
2021-06-09 00:08:53 -0400

    updated tests

--- controllers/logsController.test.js
+++ controllers/logsController.test.js
@@ -1,6 +1,6 @@
 const request = require("supertest");
 
-const logs = require("./logsController");
+const logs = require("../app.js");
 let logsArray = require("../models/log.js");
 
 describe("logs", () => {
@@ -10,24 +10,24 @@ describe("logs", () => {
     logsArray = originalLogsArray;
   });
 
-  describe("/", () => {
+  describe("/logs", () => {
     it("sends the logs array", async () => {
-      const response = await request(logs).get("/");
+      const response = await request(logs).get("/logs");
 
       expect(JSON.parse(response.text)).toEqual(logsArray);
     });
   });
 
-  describe("/:arrayIndex", () => {
+  describe("/logs/:arrayIndex", () => {
     describe("GET", () => {
       it("sends the corresponding log when a valid index is given", async () => {
-        const response = await request(logs).get("/1");
+        const response = await request(logs).get("/logs/1");
 
         expect(JSON.parse(response.text)).toEqual(logsArray[1]);
       });
 
       it("sends a redirect when an invalid index is given", async () => {
-        const response = await request(logs).get("/9001");
+        const response = await request(logs).get("/logs/9001");
 
         expect(response.redirect).toBe(true);
       });
@@ -35,54 +35,61 @@ describe("logs", () => {
 
     describe("PUT", () => {
       it("replaces the index in the logs array", async () => {
-        const newBook = logsArray[3];
+        const updatedLog = logsArray[0];
 
-        await new Promise(resolve => {
+        await new Promise((resolve) => {
           request(logs)
-            .put("/1")
-            .send(newBook)
+            .put("/logs/0")
+            .send(updatedLog)
             .set("Accept", "application/json")
-            .expect("headers.location", "/logs/1")
+            .expect("headers.location", "/logs/")
             .expect("statusCode", 303)
             .end(resolve);
         });
 
-        expect(logsArray[1]).toEqual(newBook);
+        expect(logsArray[0]).toEqual(updatedLog);
       });
     });
 
     describe("POST", () => {
       it("creates at the index in the logs array", async () => {
-        const newBook = logsArray[3];
+        const newLastArrayPosition = logsArray.length;
+        const newLog = {
+          captainName: "Picard",
+          title: "Stars",
+          post: "Today I contemplated that there sure are a lot of stars in the sky",
+          mistakesWereMadeToday: true,
+          daysSinceLastCrisis: "10",
+        };
 
-        await new Promise(resolve => {
+        await new Promise((resolve) => {
           request(logs)
-            .post("/1")
-            .send(newBook)
+            .post(`/logs`)
+            .send(newLog)
             .set("Accept", "application/json")
             .expect("headers.location", "/logs")
             .expect("statusCode", 303)
             .end(resolve);
         });
 
-        expect(logsArray[1]).toEqual(newBook);
+        expect(logsArray[newLastArrayPosition]).toEqual(newLog);
       });
     });
 
     describe("DELETE", () => {
-      it("creates at the index in the logs array", async () => {
-        const nextBook = logsArray[2];
+      it("deletes at the index in the logs array", async () => {
+        const logToDelete = logsArray[2];
         const originalLength = logsArray.length;
-        await new Promise(resolve => {
+        await new Promise((resolve) => {
           request(logs)
-            .delete("/1")
+            .delete("/logs/2")
             .set("Accept", "application/json")
             .expect("headers.location", "/logs")
             .expect("statusCode", 303)
             .end(resolve);
         });
 
-        expect(logsArray[1]).toEqual(nextBook);
+        expect(logsArray[2]).toEqual(originalLogsArray[2]);
         expect(logsArray).toHaveLength(originalLength - 1);
       });
     });
